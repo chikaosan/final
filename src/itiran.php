cffec695ec7d47c811f0de70799815b5b3d57de0
chikaosan <manayama1125@yahoo.com>
2024-01-12 12:30:12 +0900

    Update itiran.php

--- src/itiran.php
+++ src/itiran.php
@@ -9,6 +9,28 @@
 
 <body>
     <h1>楽曲一覧</h1>
+    <table>
+        <tr>
+            <th>音楽ID</th>
+            <th>曲名</th>
+            <th>アーティスト名</th>
+            <th>年代</th>
+        </tr>
+
+        <?php
+        $pdo = new PDO($connect, USER, PASS);
+        foreach ($pdo->query('select * from music') as $row) {
+            echo '<tr>';
+            echo '<td>', $row['musicid'], '</td>';
+            echo '<td>', $row['musicname'], '</td>';
+            echo '<td>', $row['artist'], '</td>';
+            echo '<td>', $row['era'], '</td>';
+            echo '</td>';
+            echo '</tr>';
+            echo "\n";
+        }
+        ?>
+
 </body>
 
 </html>
