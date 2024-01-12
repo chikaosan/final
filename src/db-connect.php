9c48c75f1bf755eab2dc1c48b354099bb7e6df73
chikaosan <manayama1125@yahoo.com>
2024-01-12 12:12:55 +0900

    Create db-connect.php

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql214.phy.lolipop.lan';
+const DBNAME = 'LAA1517339-final';
+const USER = 'LAA1517339';
+const PASS = '2201394';
+
+$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
