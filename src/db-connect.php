e428dec3460e6f0fbe30f3f1d1198ce019928af4
chikaosan <manayama1125@yahoo.com>
2024-01-12 12:43:43 +0900

    Update db-connect.php

--- src/db-connect.php
+++ src/db-connect.php
@@ -2,7 +2,7 @@
 const SERVER = 'mysql214.phy.lolipop.lan';
 const DBNAME = 'LAA1517339-final';
 const USER = 'LAA1517339';
-const PASS = '2201394';
+const PASS = 'LAA1517339';
 
 $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
 ?>
