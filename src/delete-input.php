720c5bb93ff6cfd7ad4dadebfa3608ab954e73d1
chikaosan <manayama1125@yahoo.com>
2024-01-15 10:27:51 +0900

    Create delete-input.php

---
+++ src/delete-input.php
@@ -0,0 +1,24 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517339-final';
+const USER = 'LAA1517339';
+const PASS = '2201394';
+
+$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>楽曲削除</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
