6eeab5a4fb70a294a2415c58cec619c9909b7d97
chikaosan <manayama1125@yahoo.com>
2024-01-15 09:28:48 +0900

    Update db-connect.php

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,5 +1,5 @@
 <?php
-const SERVER = 'mysql214.phy.lolipop.lan';
+const SERVER = 'mysql220.phy.lolipop.lan';
 const DBNAME = 'LAA1517339-final';
 const USER = 'LAA1517339';
 const PASS = 'LAA1517339';
