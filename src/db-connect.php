68794d7940f9445aebd9e98d373630b4ff7a0089
chikaosan <manayama1125@yahoo.com>
2024-01-15 09:48:04 +0900

    Delete db-connect.php

--- src/db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-const SERVER = 'mysql220.phy.lolipop.lan';
-const DBNAME = 'LAA1517339-final';
-const USER = 'LAA1517339';
-const PASS = '2201394';
-
-$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
-?>
