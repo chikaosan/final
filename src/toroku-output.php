1b8a86f1edd59baf72763763a2208946efb3c505
chikaosan <manayama1125@yahoo.com>
2024-01-15 10:13:27 +0900

    Update toroku-output.php

--- src/toroku-output.php
+++ src/toroku-output.php
@@ -19,26 +19,27 @@ $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
 <body>
     <?php
     $pdo = new PDO($connect, USER, PASS);
-    $sql = $pdo->prepare('select * from music where musicid=?');
-    $sql->execute([$_POST['musicid']]);
-    if (empty($sql->fetchAll())) {
-        $sql = $pdo->prepare('insert into music values (null,?,?,?,?,?,?)');
-        $sql->execute([
-            $_POST['musicid'],
-            $_POST['musicname'],
-            $_POST['artistname'],
-            $_POST['era'],
-        ]);
-        echo '<div class="CustomerTable">';
-        echo '楽曲名:', $_POST['musicid'];
-        echo '</br>';
-        echo '曲名:', $_POST['musicname'];
-        echo '</br>';
-        echo 'アーティスト名:', $_POST['artist'];
-        echo '</br>';
-        echo '年代:', $_POST['era'];
-        echo '</div>';
+    $sql = $pdo->prepare('insert into product(musicname, artistname, era)values (?, ?, ?)');
+    if (empty($_POST['musicname'])) {
+        echo '曲名を入力してください。';
+    } elseif (empty($_POST['artistname'])) {
+        echo 'アーティスト名を入力してください。';
+    } elseif (!preg_match('/^\d+$/', $_POST['era'])) {
+        echo '年代を整数で入力してください。';
+    } elseif ($sql->execute([$_POST['musicname'], $_POST['artistname'], $_POST['era']])) {
+        echo '<font color="red">追加に成功しました。</font>';
+    } else {
+        echo '<font color="red">追加に失敗しました。</font>';
     }
+    echo '<div class="CustomerTable">';
+    echo '楽曲名:', $_POST['musicid'];
+    echo '</br>';
+    echo '曲名:', $_POST['musicname'];
+    echo '</br>';
+    echo 'アーティスト名:', $_POST['artist'];
+    echo '</br>';
+    echo '年代:', $_POST['era'];
+    echo '</div>';
     ?>
     <div class="wrapper">
 
