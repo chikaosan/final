730f69473f957219d884e06c243200c890beb348
chikaosan <manayama1125@yahoo.com>
2024-01-16 15:01:30 +0900

    Update toroku-input.php

--- src/toroku-input.php
+++ src/toroku-input.php
@@ -4,30 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/toroku.css">
     <title>楽曲登録</title>
 </head>
 
 <body>
+    <div class="aa">
+        <div class="nyuryoku">
+            <header>
+                <h1>楽曲登録</h1>
+            </header>
 
-    <h1>楽曲登録</h1>
-
-    <form action="toroku-output.php" method="post">
-        <input type="text" name="musicname" id="musicname" placeholder="楽曲名">
-        </br>
-        <input type="text" name="artistname" id="artistname" placeholder="アーティスト名">
-        </br>
-        <input type="text" name="era" id="era" placeholder="年代">
-        </br>
-        <div class="wrapper">
-            <div class="toroku">
-                <input type="submit" value="登録">
-            </div>
-    </form>
-    <form action="menu.php" method="post">
-        <div class="back">
-            <input type="submit" value="戻る">
+            <form action="toroku-output.php" method="post">
+                <input type="text" name="musicname" id="musicname" placeholder="楽曲名">
+                </br>
+                <input type="text" name="artistname" id="artistname" placeholder="アーティスト名">
+                </br>
+                <input type="text" name="era" id="era" placeholder="年代">
+                </br>
+                <div class="wrapper">
+                    <div class="toroku">
+                        <input type="submit" value="登録">
+                    </div>
+            </form>
+            <form action="menu.php" method="post">
+                <div class="back">
+                    <input type="submit" value="戻る">
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
+    </div>
 
 
 </body>
