a24ed68e40b73ae6b896cee171fda9436f3cb02e
chikaosan <manayama1125@yahoo.com>
2024-01-16 14:23:33 +0900

    Update itiran.php

--- src/itiran.php
+++ src/itiran.php
@@ -13,38 +13,50 @@ $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/itiran.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>楽曲一覧</h1>
-    <table>
-        <tr>
-            <th>音楽ID</th>
-            <th>曲名</th>
-            <th>アーティスト名</th>
-            <th>年代</th>
-        </tr>
+    <div class="itiran">
+        <div class="a">
+            <header>
+                <h1>
+                    楽曲一覧
+                </h1>
+            </header>
 
-        <?php
-        $pdo = new PDO($connect, USER, PASS);
-        foreach ($pdo->query('select * from music') as $row) {
-            echo '<tr>';
-            echo '<td>', $row['musicid'], '</td>';
-            echo '<td>', $row['musicname'], '</td>';
-            echo '<td>', $row['artistname'], '</td>';
-            echo '<td>', $row['era'], '</td>';
-            echo '</td>';
-            echo '</tr>';
-            echo "\n";
-        }
-        ?>
-    </table>
+            <table>
+                <tr>
+                    <th>音楽ID</th>
+                    <th>曲名</th>
+                    <th>アーティスト名</th>
+                    <th>年代</th>
+                </tr>
 
-    <form action="menu.php" method="post">
-        <button type="submit" name="itiran">戻る
-        </button>
-    </form>
+                <?php
+                $pdo = new PDO($connect, USER, PASS);
+                foreach ($pdo->query('select * from music') as $row) {
+                    echo '<tr>';
+                    echo '<td>', $row['musicid'], '</td>';
+                    echo '<td>', $row['musicname'], '</td>';
+                    echo '<td>', $row['artistname'], '</td>';
+                    echo '<td>', $row['era'], '</td>';
+                    echo '</td>';
+                    echo '</tr>';
+                    echo "\n";
+                }
+                ?>
+            </table>
+            <div class="wrapper">
+                <form action="menu.php" method="post">
+                    <div class="back">
+                        <input type="submit" value="戻る"></input>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 </body>
